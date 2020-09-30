283d362be85b6d068263219c4e614689bf3fea81
Garman_K <garnetbirthstone@hotmail.com>
2020-09-30 19:24:17 -0400

    Click event 'Generate Password' display value

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ var validLen = parseInt(passwordLen);
 //Checking for valid int input
 if ( !validLen || validLen < 8 || validLen > 128){
   alert("Sorry, please choose a password length from 8-128.");
-  
 }
 else{
   console.log(validLen);
@@ -14,55 +13,54 @@ else{
   //Uppercase
   var hasUpper = confirm('Do you want uppercase characters?');
   if (hasUpper) charSet.push("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-  console.log(charSet);
+  
 
   //Lowercase
   var hasLower = confirm('Do you want lowercase characters?');
   if (hasLower) charSet.push("abcdefghijklmnopqrstuvwxyz");
-  console.log(charSet);
+  
 
   //Numeric
   var hasNum = confirm('Do you want numeric characters?');
   if (hasNum) charSet.push("0123456789");
-  console.log(charSet);
+
 
   //Symbols
   var hasSymbols = confirm('Do you want special characters?');
   if (hasSymbols) charSet.push( "!@#$%^&*()");
-  console.log(charSet);
+  
 
-  console.log(charSet.join(''));
+  charSet.join('-')
+  console.log(charSet);
 
   //Checking if at least one character set is selected
   if (charSet == ''){
     alert("You must choose at least one character set.")
   }
 
+}
+//slice a random character from each charSet index (string of characters)
+//var validLen is password length
 
+function createPassword (){
+  for (let i = 0; i < validLen; i++) {
+    charSet[i]
+    
+  }
 
 }
+createPassword()
 
 
+//Generate button to display password
 
+document.getElementById("generate").addEventListener("click", function() {
+  document.getElementById("password").value = charSet;
+});
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  for (var i = 0; i < validLen; i++){
+/*   for (var i = 0; i < validLen; i++){
     var uniquePass = Math.floor(Math.random() * charSet);
   }
 
   passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-function generatePassword(){
-  generateBtn.addEventListener("click", writePassword);
-
-}
+ */
